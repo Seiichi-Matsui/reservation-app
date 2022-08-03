5e910e8c8a39e2b234005ea6bb9d545cd47b7c76
Seiichi-Matsui <s-matsui@zima-corp.com>
2022-08-03 14:09:35 +0900

    ヘッダーコンポーネント追加

---
+++ src/app/common/navbar/navvar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navvar.component.html',
+  styleUrls: ['./navvar.component.scss']
+})
+export class NavbarComponent {
+}
